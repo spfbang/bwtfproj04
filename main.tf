6af9cbe5b38419d90fe5fd49e7f39dee1354d5b9
spfbang <140609302+spfbang@users.noreply.github.com>
2024-09-11 17:02:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,42 @@
-fsdfhkdfhkhfkdshf24234y2347294239479247932
+# Set the Azure Provider source and version being used
+terraform {
+  required_version = ">= 0.14"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.1.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure provider
+provider "azurerm" {
+  features {}
+}
+
+# Create a Resource Group if it doesn’t exist
+resource "azurerm_resource_group" "tfexample" {
+  name     = "BluWood-rg"
+  location = "West Europe"
+}
+
+# Create a Virtual Network
+resource "azurerm_virtual_network" "tfexample" {
+  name                = "BW-vnet"
+  location            = azurerm_resource_group.tfexample.location
+  resource_group_name = azurerm_resource_group.tfexample.name
+  address_space       = ["10.0.0.0/16"]
+
+  tags = {
+    environment = "my-terraform-env"
+  }
+}
+
+# Create a Subnet in the Virtual Network
+resource "azurerm_subnet" "tfexample" {
+  name                 = "BW-Subnet"
+  resource_group_name  = azurerm_resource_group.tfexample.name
+  virtual_network_name = azurerm_virtual_network.tfexample.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
